0297a849ab8b0a2028832a3552c5332b69f0c5f6
netfj <netfj@sina.com>
2018-12-06 18:56:52 +0800

    2018-12-05 Insatll V0.0

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\微云同步助手\1571257122\QQ网盘\FjUse\Python\Django_stu01\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
